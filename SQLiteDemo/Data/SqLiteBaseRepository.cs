bba1f2f7b5dc126c80e975cb4dc0c67c06dbbb69
xdqt <849773373@qq.com>
2021-07-06 17:26:00 +0800

    连接池

--- SQLiteDemo/Data/SqLiteBaseRepository.cs
+++ SQLiteDemo/Data/SqLiteBaseRepository.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using Microsoft.Data.Sqlite;
 
 namespace SQLiteDemo.Data
@@ -24,6 +27,139 @@ namespace SQLiteDemo.Data
             return new SqliteConnection(conn);//创建SQLite连接
 
             
+        }
+    }
+
+
+    //使用连接池连接
+    public static class SqliteConnectionPool
+    {
+        private const int PoolSize = 10;
+
+        private static readonly object Locker = new object();
+
+        static SqliteConnectionPool()
+        {
+            Pool = new List<DbConnection>(PoolSize);
+            Additional = new List<DbConnection>();
+        }
+        public static string DbFile
+        {
+            get { return Environment.CurrentDirectory + "\\SimpleDb.sqlite"; }
+        }
+        private static bool Finalized { get; set; }
+
+        private static int CreatedConnectionsMax => PoolSize;
+        private static int CreatedConnectionsCount { get; set; }
+
+        private static List<DbConnection> Pool { get; }
+        private static List<DbConnection> Additional { get; }
+
+        public static DbConnection GetConnection()
+        {
+            if (Finalized)
+            {
+                throw new Exception("Connection pool was finalized.");
+            }
+
+            lock (Locker)
+            {
+                if (Pool.Count == 0)
+                {
+                    var connection = CreateAndOpenConnection();
+                    if (CreatedConnectionsCount == CreatedConnectionsMax)
+                    {
+                        Additional.Add(connection);
+                        LogAdditionalConnection();
+                        return connection;
+                    }
+                    else
+                    {
+                        CreatedConnectionsCount++;
+                        Pool.Add(connection);
+                        return connection;
+                    }
+                }
+                else
+                {
+                    var connection = Pool[0];
+                    Pool.RemoveAt(0);
+                    return connection;
+                }
+            }
+        }
+
+        public static void ReleaseConnection(DbConnection connection)
+        {
+            if (Finalized)
+            {
+                CloseAndDisposeConnection(connection);
+                return;
+            }
+
+            lock (Locker)
+            {
+                if (Additional.Contains(connection))
+                {
+                    Additional.Remove(connection);
+                    CloseAndDisposeConnection(connection);
+                }
+                else
+                {
+                    Pool.Add(connection);
+                }
+            }
+        }
+
+        public static void Dispose()
+        {
+            lock (Locker)
+            {
+                foreach (var connection in Pool)
+                {
+                    CloseAndDisposeConnection(connection);
+                }
+
+                Finalized = true;
+            }
+        }
+
+        private static DbConnection CreateAndOpenConnection()
+        {
+            // create connection string
+
+            var connStr = @"Data Source=" + DbFile;//连接字符串
+
+            var connectionString = new SqliteConnectionStringBuilder(connStr)
+            {
+                Mode = SqliteOpenMode.ReadWriteCreate,
+                Password = "password"
+            }.ToString();//使用这个方式设置密码，避免sql注入
+            // create and open connection
+            var conn = new SqliteConnection(connectionString);
+            conn.Open();
+
+            // enable write-ahead log
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "PRAGMA journal_mode = 'wal'";
+            cmd.ExecuteNonQuery();
+
+            return conn;
+        }
+
+        private static void CloseAndDisposeConnection(DbConnection connection)
+        {
+            if (connection.State != ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+            connection.Dispose();
+        }
+
+        private static void LogAdditionalConnection()
+        {
+            // it's important to know if your code is frequently opening additional connections
+            // that way you can increase the pool size to improve performance
         }
     }
 }
--- SQLiteDemo/Data/SqLiteCustomerRepository.cs
+++ SQLiteDemo/Data/SqLiteCustomerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Data.Common;
+using System.IO;
 using System.Linq;
 using Dapper;
 using Microsoft.Data.Sqlite;
@@ -8,48 +9,56 @@ namespace SQLiteDemo.Data
 {
     public class SqLiteCustomerRepository<T> : SqLiteBaseRepository, ICustomerRepository<T>
     {
-        public static SqliteConnection cnn = SimpleDbConnection();
 
-        public static void con ()
-        {
-            cnn.Open();
-        }
-        public T Get<T>(long id,string sql)
+
+        public T Get<T>(long id, string sql)
         {
             if (!File.Exists(DbFile)) return default(T);
 
+            var cnn = SqliteConnectionPool.GetConnection();
 
-                T result = cnn.Query<T>(
-                    sql, new { id }).FirstOrDefault();
-                return result;
-            
+            T result = cnn.Query<T>(
+               sql, new { id }).FirstOrDefault();
+
+
+
+            SqliteConnectionPool.ReleaseConnection(cnn);
+            return result;
         }
 
-        public void Save(T customer,string sql)
+        public void Save(T customer, string sql)
         {
 
-          SqliteTransaction sqliteTransaction=   cnn.BeginTransaction();
-            try
-            {
-                cnn.Query<long>(
-                    sql, customer).First();
-                sqliteTransaction.Commit();
-            }
-            catch (System.Exception)
-            {
 
-                sqliteTransaction.Rollback();
+
+            var cnn = SqliteConnectionPool.GetConnection();
+            {
+                DbTransaction sqliteTransaction = cnn.BeginTransaction();
+                try
+                {
+                    cnn.Query<long>(
+                    sql, customer).First();
+                    sqliteTransaction.Commit();
+                }
+                catch (System.Exception)
+                {
+
+                    sqliteTransaction.Rollback();
+                }
             }
-            
+            SqliteConnectionPool.ReleaseConnection(cnn);
 
         }
 
         public static void CreateDatabase(string createdb)
         {
-            con();
+
+            var cnn = SqliteConnectionPool.GetConnection();
+            {
                 cnn.Execute(createdb
                     );
-            
+            }
+            SqliteConnectionPool.ReleaseConnection(cnn);
         }
     }
 }
